3f152808003ce3ab7973427e7dd6833f47f7b61e
Imran Mondal <hello@imranmondal.com>
2017-01-28 05:33:15 +0600

    Done & Deployed

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,10 +3,8 @@
  * Documentation: https://openweathermap.org/current         *
  * --------------------------------------------------------- */
 
-var api       = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?",
+var api       = "http://api.openweathermap.org/data/2.5/weather?",
 	appid     = "appid=e0b01df8d539332186b67b9610fdd7b2",
-	lon       = "lon=",
-	lat       = "lat=",
 	temp      = $("#temp"),
 	indicator = $("#indicator"),
 	city      = $("#city"),
@@ -14,77 +12,78 @@ var api       = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/w
 	sunSet    = $("#sun-set"),
 	appuri;
 
+$(document).ready(function($) {
+	
+	//Get Location
+	$.ajax({
+		url: 'http://ipinfo.io/',
+		type: 'GET',
+		dataType: 'json',
+	})
 
-$("document").ready(function() {
-	// Obtaing Lattitude & longitude from user
-	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function (position) {
-			lon += position.coords.longitude;
-			lat += position.coords.latitude;
-			apiuri     = api + "&" + lon + "&" + lat + "&" + "units=metric" + "&" + appid;
+	.done(function(location) {
+		var place   = location.city,
+			apiuri = api + "q=" + place + "&" + "units=metric" + "&" + appid;
 
-			// Perfoming AJAX request to API server
-			$.ajax({
-				url: apiuri,
-				type: 'GET', jsonp: "callback",
-				dataType: 'jsonp',
-			})
+		$.ajax({
+			url: apiuri,
+			type: 'GET',
+			dataType: 'json',
+		})
 
-			// Update Data On page if we successfully communicate with server
-			.done(function(data) {
-				temp.text(data.main.temp);
-				indicator.text("°C");
-				city.text(data.name);
-				console.log(data);
+		// Update Data On page if we successfully communicate with server
+		.done(function(data) {
+			temp.text(Math.round(data.main.temp));
+			indicator.text("°C");
+			city.text(place);
 
-				// Convert Unix Time to Local GMT
-				var sunRiseTime = new Date(data.sys.sunrise*1000).toLocaleTimeString(),
-				    sunSetTime 	= new Date(data.sys.sunset*1000).toLocaleTimeString();
+			// Convert Unix Time to Local GMT
+			var sunRiseTime = new Date(data.sys.sunrise*1000).toLocaleTimeString(),
+				sunSetTime 	= new Date(data.sys.sunset*1000).toLocaleTimeString();
 
-				sunRise.text(sunRiseTime);
-				sunSet.text(sunSetTime);
+			sunRise.text(sunRiseTime);
+			sunSet.text(sunSetTime);
 
-				// Adding Icon
-				var weatherCon,
-					weatherId = (data.weather[0].id);
-					if (weatherId > 199 && weatherId < 322) {
-						weatherCon = "thunderstom";
-					}
-					if (weatherId > 499 && weatherId < 532) {
-						weatherCon = "rain";
-					}
-					if (weatherId > 599 && weatherId < 623) {
-						weatherCon = "snow";
-					}
-					if (weatherId > 700 && weatherId < 782) {
-						weatherCon = "clouds";
-					}
-					if (weatherId === 800) {
-						weatherCon = "clear";
-					}
-					if (weatherId > 800 && weatherId < 805) {
-						weatherCon = "clouds";
-					}
-					if (weatherId > 899 && weatherId < 907) {
-						weatherCon = "thunderstom";
-					}
-					else {
-						weatherCon = "clear";
-					}
-
-					function addIcon(weatherIcon) {
-						$('.' + weatherIcon).removeClass('hide');
-					};
-					addIcon(weatherCon);
-			})
-
-			// Show Error Massage if something went Wrong
-			.fail(function() {
-				console.log("error");
-			});
+			// Adding Icon
+			var weatherCon,
+				weatherId = (data.weather[0].id);
 			
-		});
-	};
+			if (weatherId > 199 && weatherId < 322) {
+				weatherCon = "thunderstom";
+				}
+			if (weatherId > 499 && weatherId < 532) {
+				weatherCon = "rain";
+				}
+			if (weatherId > 599 && weatherId < 623) {
+				weatherCon = "snow";
+				}
+			if (weatherId > 700 && weatherId < 782) {
+				weatherCon = "clouds";
+				}
+			if (weatherId === 800) {
+				weatherCon = "clear";
+				}
+			if (weatherId > 800 && weatherId < 805) {
+				weatherCon = "clouds";
+				}
+			if (weatherId > 899 && weatherId < 907) {
+				weatherCon = "thunderstom";
+				}
+			else {
+				weatherCon = "clear";
+				}
 
+			function addIcon(weatherIcon) {
+				$('.' + weatherIcon).removeClass('hide');
+			};
+			addIcon(weatherCon);
+		})
+
+		// Show Error Massage if something went Wrong
+		.fail(function() {
+			console.log("error");
+		});
+		
+	});
 	
 });
